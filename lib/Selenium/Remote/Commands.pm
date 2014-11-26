afd21818db3321e6f30d1b176debf5b0098f735a
Daniel Gempesaw <gempesaw@gmail.com>
2014-11-26 16:39:26 -0500

    Build results of e565942 (on master)

--- lib/Selenium/Remote/Commands.pm
+++ lib/Selenium/Remote/Commands.pm
@@ -1,5 +1,5 @@
 package Selenium::Remote::Commands;
-$Selenium::Remote::Commands::VERSION = '0.2201';
+$Selenium::Remote::Commands::VERSION = '0.2202';
 # ABSTRACT: Implement commands for Selenium::Remote::Driver
 
 use Moo;
@@ -426,7 +426,7 @@ Selenium::Remote::Commands - Implement commands for Selenium::Remote::Driver
 
 =head1 VERSION
 
-version 0.2201
+version 0.2202
 
 =head1 SEE ALSO
 
