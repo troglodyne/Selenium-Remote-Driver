e30fde2c0e48028c088632a2467f10139b7bba2b
Daniel Gempesaw <gempesaw@gmail.com>
2015-03-24 09:17:08 -0400

    Build results of 825dc2c (on master)

--- lib/Test/Selenium/Remote/Driver.pm
+++ lib/Test/Selenium/Remote/Driver.pm
@@ -1,5 +1,5 @@
 package Test::Selenium::Remote::Driver;
-$Test::Selenium::Remote::Driver::VERSION = '0.24';
+$Test::Selenium::Remote::Driver::VERSION = '0.2450'; # TRIAL
 # ABSTRACT: Useful testing subclass for Selenium::Remote::Driver
 
 use Moo;
