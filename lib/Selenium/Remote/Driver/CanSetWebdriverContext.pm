9ab013124d87a562c27762232c8c8a112222ac33
Daniel Gempesaw <gempesaw@gmail.com>
2016-01-11 16:44:59 -0500

    Build results of 71f5762 (on master)

--- lib/Selenium/Remote/Driver/CanSetWebdriverContext.pm
+++ lib/Selenium/Remote/Driver/CanSetWebdriverContext.pm
@@ -3,6 +3,7 @@ $Selenium::Remote::Driver::CanSetWebdriverContext::VERSION = '0.28';
 # ABSTRACT: Customize the webdriver context prefix for various drivers
 use Moo::Role;
 
+
 has 'wd_context_prefix' => (
     is => 'lazy',
     default => sub { '/wd/hub' }
@@ -23,6 +24,19 @@ Selenium::Remote::Driver::CanSetWebdriverContext - Customize the webdriver conte
 =head1 VERSION
 
 version 0.28
+
+=head1 DESCRIPTION
+
+Some drivers don't use the typical C</wd/hub> context prefix for the
+webdriver HTTP communication. For example, the newer versions of the
+Firefox driver extension use the context C</hub> instead. This role
+just has the one attribute with a default webdriver context prefix,
+and is consumed in L<Selenium::Remote::Driver> and
+L<Selenium::Remote::RemoteConnection>.
+
+If you're new to webdriver, you probably want to head over to
+L<Selenium::Remote::Driver>'s docs; this package is more of an
+internal-facing concern.
 
 =head1 SEE ALSO
 
