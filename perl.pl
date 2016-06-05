150c542d1023fdfe495fb1256b3dd8fd37ab5cd6
Daniel Gempesaw <gempesaw@gmail.com>
2016-06-05 10:08:06 -0400

    Build results of ffa72d8 (on master)

--- perl.pl
+++ perl.pl
@@ -1,13 +1,14 @@
 use strict;
 use warnings;
 use Test::More;
-use lib('./lib/');
+use lib 'lib';
 use Test::Selenium::Remote::Driver;
 
 my $driver = Test::Selenium::Remote::Driver->new(
     error_handler => sub { print 'whee' }
 );
+
 $driver->get('https://www.google.com');
-$driver->content_like(qr/noooowjefpiawjpefiwajewpf/);
+$driver->find_no_element_ok('asdf', 'css');
 
 done_testing;
