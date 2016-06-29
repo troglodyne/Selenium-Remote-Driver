b2c19f600482f8fada6f611d6e8c57b0fc049583
Daniel Gempesaw <gempesaw@gmail.com>
2016-06-29 11:56:15 -0400

    Build results of 71c1444 (on master)

--- lib/Selenium/Waiter.pm
+++ lib/Selenium/Waiter.pm
@@ -24,8 +24,9 @@ sub wait_until (&%) {
 
     my $exception = '';
     while ($timeout_not_elapsed->()) {
+        my $assert_ret;
         my $try_ret = try {
-            my $assert_ret = $assert->();
+            $assert_ret = $assert->();
             return $assert_ret if $assert_ret;
         }
         catch {
@@ -34,7 +35,9 @@ sub wait_until (&%) {
             return '';
         }
         finally {
-            sleep($args->{interval});
+            if (! $assert_ret) {
+                sleep($args->{interval});
+            }
         };
 
         return $try_ret if $try_ret;
