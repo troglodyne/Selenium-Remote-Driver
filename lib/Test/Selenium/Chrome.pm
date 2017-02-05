85942a6fe21f895a8b018cd7947db660076ab3a2
Daniel Gempesaw <gempesaw@gmail.com>
2017-02-05 17:35:03 -0500

    Build results of 1a01328 (on master)

--- lib/Test/Selenium/Chrome.pm
+++ lib/Test/Selenium/Chrome.pm
@@ -1,5 +1,5 @@
 package Test::Selenium::Chrome;
-$Test::Selenium::Chrome::VERSION = '1.11';
+$Test::Selenium::Chrome::VERSION = '1.12';
 use Moo;
 extends 'Selenium::Chrome', 'Test::Selenium::Remote::Driver';
 
--- lib/Test/Selenium/PhantomJS.pm
+++ lib/Test/Selenium/PhantomJS.pm
@@ -1,5 +1,5 @@
 package Test::Selenium::PhantomJS;
-$Test::Selenium::PhantomJS::VERSION = '1.11';
+$Test::Selenium::PhantomJS::VERSION = '1.12';
 use Moo;
 extends 'Selenium::PhantomJS', 'Test::Selenium::Remote::Driver';
 
